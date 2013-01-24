a1f40539829c2f26f8178f4d97796cac1cea4f4a
hindavi <anandjoshi@hindavi.in>
2013-01-24 16:40:34 +0530

    user role method added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  def user_role
+     @user = User.find_by_id(current_user.id)
+     @role = @user.roles
+     if @role[0].role_type=="Admin" || @role[0].role_type=="Super User" 
+       return true
+     else
+        respond_to do |formate|
+          formate.html{redirect_to :action=>"index",:controller=>"home" }
+        end	
+     end	
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+	before_filter :user_role ,:except=>"index"
   def index
+
   end
 end
