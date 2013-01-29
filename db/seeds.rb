e6e6c75010ef2ea56a01a7578c315b45ecc09329
creativity <smita.lawande07@gmail.com>
2013-01-29 14:38:35 +0530

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(:email=>"admin@hindavi.in", :password=>"admin@h", :password_confirmation=>"admin@h",  :status=>"Active", :username=>"hindavi", :role_id =>1)
-Role.create!([{ role_type: 'admin' },	{ role_type: 'user' }])
+User.create(:email=>"admin@hindavi.in", :password=>"admin@h", :password_confirmation=>"admin@h",  :status=>"Active", :username=>"hindavi", :role_id =>1)
+Role.create([{ role_type: 'admin' },	{ role_type: 'user' }])
 
