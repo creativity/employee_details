ca4c87b5e8968c7754ac0681ab33e30eb16c318d
hindavi <anandjoshi@hindavi.in>
2013-01-29 19:36:39 +0530

    address model added

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -1,3 +1,6 @@
 class TeamUser < ActiveRecord::Base
   attr_accessible :team_id, :user_id
+
+  belongs_to :user
+  belongs_to :team
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 	# associations----------------------------------------------------------------------------------------
 	has_and_belongs_to_many	:roles
+  
+  has_many :team_users
+  has_many :teams, :through=>:team_users 
+ 
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -9,5 +14,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,:role,:status,:username
+
+
   # attr_accessible :title, :body
 end
