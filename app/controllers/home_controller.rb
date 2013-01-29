3302580a275f06c95ca6392382dda0e21bb394c8
creativity <smita.lawande07@gmail.com>
2013-01-29 14:30:26 +0530

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,10 @@
 class HomeController < ApplicationController
-	
 
   def index
-
      @role = current_user.role
-     if @role.role_type =="Admin" or  @role.role_type =="Super User"
+     if @role.role_type =="Admin"
        redirect_to home_admin_index_path
     end		
-  
   end
 
   def admin_index
