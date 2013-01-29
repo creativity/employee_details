30191a06d9f35078fc1ce169246534eefd03bb5b
creativity <smita.lawande07@gmail.com>
2013-01-29 14:23:06 +0530

    modifying home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
 
   def index
 
-     @role = current_user.roles
-     if @role[0].role_type =="Admin" or  @role[0].role_type =="Super User"
+     @role = current_user.role
+     if @role.role_type =="Admin" or  @role.role_type =="Super User"
        redirect_to home_admin_index_path
     end		
   
