c4707d6cd35e73aa1da9346123f827cae7d75439
creativity <smita.lawande07@gmail.com>
2013-01-31 14:47:30 +0530

    a

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ HindaviEmployee::Application.routes.draw do
 
   resources :tasks
 
-  resources :appraisal
+  resources :appraisals
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
