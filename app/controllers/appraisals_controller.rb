45eedc51d33c735f29b6110f24d4c956238ba45c
smitalawande <smita.lawande07@gmail.com>
2013-02-05 10:36:05 +0530

    adding method

--- app/controllers/appraisals_controller.rb
+++ app/controllers/appraisals_controller.rb
@@ -4,6 +4,14 @@ class AppraisalsController < ApplicationController
     @appraisal = Appraisal.new
   end
 
+  def show
+    @appraisal  = Appraisal.find(params[:id])
+  end
+
+  def edit
+    @appraisal  = Appraisal.find(params[:id])
+  end
+
   def create
     @appraisal = Appraisal.new(params[:appraisal])
     respond_to do |format|
@@ -11,6 +19,17 @@ class AppraisalsController < ApplicationController
         format.html { redirect_to @appraisal, notice: 'Appraisal was successfully created.' }
       else
         format.html { render action: "new" }
+      end
+    end
+  end
+
+  def update
+    @appraisal = Appraisal.find(params[:id])
+    respond_to do |format|
+      if @appraisal.update_attributes(params[:appraisal])
+        format.html { redirect_to @appraisal, notice: 'Leave was successfully updated.' }
+      else
+        format.html { render action: "edit" }
       end
     end
   end
--- app/models/appraisal.rb
+++ app/models/appraisal.rb
@@ -1,14 +1,18 @@
 class Appraisal < ActiveRecord::Base
 
+  # associations----------------------------------------------------------------------------------------
+  belongs_to  :user
+
   # attributes ----------------------------------------------------------------------------
   attr_accessible :date_of_joining, :description, :total_period_worked, :user_id
 
   # validations ----------------------------------------------------------------------------
   validates :date_of_joining, :presence => true
   validates :description, :presence => true
+  validate  :joining_date
 
   # callbacks ----------------------------------------------------------------------------
-  after_save  :total_days_worked
+  after_create  :total_days_worked
 
   # methods ----------------------------------------------------------------------------
   def total_days_worked
@@ -17,8 +21,19 @@ class Appraisal < ActiveRecord::Base
     start_date = Date.parse(st)
     ed = Time.now.strftime("%Y-%m-%d")
     end_date = Date.parse(ed)
-    total_period_worked = (end_date-start_date).to_i
-    return  total_period_worked
+    total_days_worked = (end_date-start_date).to_i
+    p "11111111111111111"
+    p  total_days_worked
+    self.total_period_worked = total_days_worked
+    self.save
+   # self.update_attribute("total_period_worked",total_days_worked)
+  end
+
+  def joining_date
+    unless (self.date_of_joining < (Time.now).to_date)
+      errors[:base] << "Joining date should be less than today's date"
+    end
+
   end
 
 end
